484b96847a60e77888c82fc30866cdf03ff95ad6
Abass Olaiya <ayotundeolaiya@gmail.com>
2025-04-28 08:02:49 +0100

    notWorking

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const userController = require("../controllers/userController");
 const { authMiddleware } = require("../middleware/authMiddleware");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
