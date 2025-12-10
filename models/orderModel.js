3e17f38c16ccc76bb1469ba20963efdb140d1f78
Abass Olaiya <ayotundeolaiya@gmail.com>
2025-12-10 19:35:58 +0100

    order

--- models/orderModel.js
+++ models/orderModel.js
@@ -76,8 +76,7 @@ const paymentSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    required: true,
-    // enum: ["pending", "completed", "failed", "refunded"],
+    required: true, // enum: ["pending", "completed", "failed", "refunded"],
     default: "pending",
   },
   transactionId: String,
